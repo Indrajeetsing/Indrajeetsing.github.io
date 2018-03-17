dc7846c3a1d1a6554b8999e83a5f0174e2022788
Indrajeet Singh Mandloi <indrajeet925@gmail.com>
2018-03-17 02:04:45 -0500

    Navbar completed

--- portfolio.js
+++ portfolio.js
@@ -1,15 +1,41 @@
 var myApp = angular.module("Mymodule", []).controller("myController", function ($scope){
 
+    $scope.navbarVisible = false;
     $scope.homeVisible = true;
     $scope.aboutVisible = false;
+    $scope.portfolioVisible = false;
+    $scope.contactVisible = false;
 
     $scope.homePage = function () {
+        $scope.navbarVisible = false;
         $scope.homeVisible =true;
         $scope.aboutVisible = false;
+        $scope.portfolioVisible = false;
+        $scope.contactVisible = false;
     }
 
     $scope.aboutPage = function(){
+        $scope.navbarVisible = true;
         $scope.aboutVisible = true;
         $scope.homeVisible = false;
+        $scope.portfolioVisible = false;
+        $scope.contactVisible = false;
     }
+
+    $scope.portfolioPage = function () {
+        $scope.navbarVisible = true;
+        $scope.homeVisible = false;
+        $scope.aboutVisible = false;
+        $scope.portfolioVisible = true;
+        $scope.contactVisible = false;
+    }
+
+    $scope.contactPage = function () {
+        $scope.navbarVisible = true;
+        $scope.homeVisible = false;
+        $scope.aboutVisible = false;
+        $scope.portfolioVisible = false;
+        $scope.contactVisible = true;
+    }
+
 });
